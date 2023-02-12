20c39dfd03cf5456d17c46f54209e77c0f0a6e88
EfyOliveira <114318414+EfyOliveira@users.noreply.github.com>
2023-02-12 17:49:09 -0300

    Update chat_privacy_server.py

--- chat_privacy_server.py
+++ chat_privacy_server.py
@@ -1,34 +1,51 @@
 import socket
 
-# Endereço IP e porta do servidor
-HOST = '127.0.0.1'
-PORT = 12345
+def iniciar_servidor():
+    host = 'localhost'
+    port = 12345
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    servidor.bind((host, port))
+    servidor.listen(1)
+    print("Servidor iniciado.")
+    print("Aguardando conexão...")
+    conn, addr = servidor.accept()
+    print("Conexão estabelecida com:", addr)
+    return conn
 
-# Cria o socket do servidor
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def iniciar_cliente():
+    host = 'localhost'
+    port = 12345
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((host, port))
+    print("Conectado ao servidor.")
+    return cliente
 
-# Liga o socket ao endereço IP e porta especificados
-server.bind((HOST, PORT))
+def enviar_mensagem(conn):
+    while True:
+        mensagem = input()
+        if mensagem == "sair":
+            conn.send(mensagem.encode())
+            conn.close()
+            break
+        conn.send(mensagem.encode())
 
-# Escuta por conexões
-server.listen()
+def receber_mensagem(conn):
+    while True:
+        mensagem = conn.recv(1024).decode()
+        if mensagem == "sair":
+            conn.close()
+            break
+        print("Outro usuário:", mensagem)
 
-# Aceita uma conexão e retorna um novo socket e o endereço do cliente
-conn, addr = server.accept()
+def bate_papo():
+    modo = input("Você deseja ser o servidor ou o cliente? (s/c)")
+    if modo == 's':
+        conn = iniciar_servidor()
+        enviar_mensagem(conn)
+        receber_mensagem(conn)
+    elif modo == 'c':
+        conn = iniciar_cliente()
+        receber_mensagem(conn)
+        enviar_mensagem(conn)
 
-while True:
-    # Recebe uma mensagem do cliente
-    message = conn.recv(1024).decode()
-
-    # Verifica se a mensagem é "exit" para encerrar a conexão
-    if message == 'exit':
-        break
-
-    # Imprime a mensagem
-    print(f'Mensagem recebida do cliente: {message}')
-
-    # Envia uma resposta para o cliente
-    conn.sendall(b'Mensagem recebida com sucesso!')
-
-# Fecha a conexão
-conn.close()
+bate_papo()
